a1ad772bd7d9f6b910302bd6eb4422a234308502
Pebit <129684810+Pebit@users.noreply.github.com>
2023-04-02 20:48:09 +0300

    Changed logic

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 function MeawSound()
 {
-    let chance = Math.floor(Math.random() * 5);
-    if(chance > 3)
+    let chance = Math.floor(Math.random() * 7);
+    if(chance > 5)
     {
         var audio = new Audio('SECRET.mp3');
         audio.play();
